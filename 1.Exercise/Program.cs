0f0e8dcc4a2cfb81e4272e4a46cfca3a72526c5a
Arthur <Artur.e@gmail.com>
2023-03-07 07:44:28 +0600

    Добавил решение

---
+++ 1.Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+System.Console.Write($"Введите количество элементов: ");
+
+int ElementsCount = int.Parse(Console.ReadLine());
+
+int[] MyArray = new int[ElementsCount];
+
+for (int i = 0; i < MyArray.Length; i++)
+{
+    System.Console.Write($"Введите элемент массива под индексом {i}: ");
+    MyArray[i] = int.Parse(Console.ReadLine());
+}
+
+int CountAllPositiveNumbers(int[] InputArray)
+{
+    int count = 0;
+    for (int i = 0; i < InputArray.Length; i++)
+    {
+        if (InputArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+System.Console.Write($"Цифр больше нуля:{CountAllPositiveNumbers(MyArray)}");
+
+
